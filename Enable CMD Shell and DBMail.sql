f605fc4e20c6454243705a8e6ce258df6676c870
Parvinder Nijjar <parv@parvindernijjar.com>
2019-01-03 22:59:49 +0000

    Add files via upload

---
+++ Enable CMD Shell and DBMail.sql
@@ -0,0 +1,12 @@
+sp_configure 'show advanced options',1
+go
+reconfigure with override
+go
+sp_configure 'database mail xps',1
+go
+reconfigure with override
+go
+sp_configure 'xp_cmdshell',1
+go
+reconfigure with override
+go
---
+++ SQL Reporting Services Hide Reports script.sql
@@ -0,0 +1,11 @@
+UPDATE Catalog
+SET Hidden = 1
+WHERE ItemID IN
+(
+SELECT ItemID
+FROM Catalog
+WHERE Path like '/XAFINITY/ITS/SQL2005/%'
+AND 
+( Type IN (2,5) OR Type In (1) AND [Name] = 'DataSources')
+AND [Name] <> 'performance_dashboard_main'
+)
