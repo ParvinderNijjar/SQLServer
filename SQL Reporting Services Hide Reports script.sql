b8962fa5cf38a5a1ca67b1af4eb789896423b3ca
Parvinder Nijjar <parv@parvindernijjar.com>
2019-01-17 21:12:27 +0000

    Update SQL Reporting Services Hide Reports script.sql

--- SQL Reporting Services Hide Reports script.sql
+++ SQL Reporting Services Hide Reports script.sql
@@ -4,7 +4,7 @@ WHERE ItemID IN
 (
 SELECT ItemID
 FROM Catalog
-WHERE Path like '/XAFINITY/ITS/SQL2005/%'
+WHERE Path like '/Example/directory/SQL2005/%'
 AND 
 ( Type IN (2,5) OR Type In (1) AND [Name] = 'DataSources')
 AND [Name] <> 'performance_dashboard_main'
